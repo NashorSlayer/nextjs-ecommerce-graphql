b67c9ecdd8a38bd712d5eeb19fd29205d91213ab
CRISTIIV <89605394+CRISTIIV@users.noreply.github.com>
2023-12-17 17:48:25 -0300

    Update Admin Esthetics

--- components/ShowAdmin.tsx
+++ components/ShowAdmin.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const ShowAdmin = () => {
   return (
     <section className="h-screen flex items-center justify-center">
-        <div className="bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700 text-lg">
+        <div className="bg-white shadow text-lg"style={{background: "#353734", borderColor: "#9acd1b", borderWidth: 5, borderRadius: 50}}>
           <div className="p-4 space-y-3">
             <h2 className="flex items-center justify-center text-xl font-bold text-gray-900 dark:text-white">
               Panel de Administración
@@ -14,30 +14,30 @@ const ShowAdmin = () => {
               {/* Sección Usuarios */}
               <div>
                 <p className="text-gray-700 dark:text-gray-300 font-bold">Usuarios</p>
-                <button className="mt-4 w-full bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium rounded-full text-sm px-5 py-3 text-center border-2 border-primary-600 rounded-md">Gestionar Usuarios</button>
+                <button className="mt-4 ml-10 bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium text-sm px-5 py-3 text-center border-2 border-primary-600" style={{width: "80%", backgroundColor: "#9acd1b", borderRadius: 20}}>Gestionar Usuarios</button>
                 {/* Agrega más funcionalidades de usuarios según sea necesario */}
               </div>
 
               {/* Sección Productos */}
               <div>
                 <p className="text-gray-700 dark:text-gray-300 font-bold">Productos</p>
-                <button className="mt-4 w-full bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium rounded-full text-sm px-5 py-3 text-center border-2 border-primary-600 rounded-md">Editar Información de Productos</button>
-                <button className="mt-2 w-full bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium rounded-full text-sm px-5 py-3 text-center border-2 border-primary-600 rounded-md">Añadir Stock</button>
+                <button className="mt-4 ml-10 bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium text-sm px-5 py-3 text-center border-2 border-primary-600" style={{width: "80%", backgroundColor: "#9acd1b", borderRadius: 20}}>Editar Información de Productos</button>
+                <button className="mt-4 ml-10 bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium text-sm px-5 py-3 text-center border-2 border-primary-600" style={{width: "80%", backgroundColor: "#9acd1b", borderRadius: 20}}>Añadir Stock</button>
                 {/* Agrega más funcionalidades de productos según sea necesario */}
               </div>
 
               {/* Sección Categorías */}
               <div>
                 <p className="text-gray-700 dark:text-gray-300 font-bold">Categorías</p>
-                <button className="mt-4 w-full bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium rounded-full text-sm px-5 py-3 text-center border-2 border-primary-600 rounded-md">Editar Categorías</button>
+                <button className="mt-4 ml-10 bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium text-sm px-5 py-3 text-center border-2 border-primary-600" style={{width: "80%", backgroundColor: "#9acd1b", borderRadius: 20}}>Editar Categorías</button>
                 {/* Agrega más funcionalidades de categorías según sea necesario */}
               </div>
 
               {/* Sección Cupones */}
               <div>
                 <p className="text-gray-700 dark:text-gray-300 font-bold">Cupones</p>
-                <button className="mt-4 w-full bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium rounded-full text-sm px-5 py-3 text-center border-2 border-primary-600 rounded-md">Añadir Cupones</button>
-                <button className="mt-2 w-full bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium rounded-full text-sm px-5 py-3 text-center border-2 border-primary-600 rounded-md">Eliminar / Deshabilitar Cupones</button>
+                <button className="mt-4 ml-10 bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium text-sm px-5 py-3 text-center border-2 border-primary-600" style={{width: "80%", backgroundColor: "#9acd1b", borderRadius: 20}}>Añadir Cupones</button>
+                <button className="mt-4 ml-10 bg-gradient-to-r from-primary-500 to-primary-600 hover:from-primary-600 hover:to-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 text-white font-medium text-sm px-5 py-3 text-center border-2 border-primary-600" style={{width: "80%", backgroundColor: "#9acd1b", borderRadius: 20}}>Eliminar / Deshabilitar Cupones</button>
                 {/* Agrega más funcionalidades de cupones según sea necesario */}
               </div>
 
